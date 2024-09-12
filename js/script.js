ff8cc6989a9231aaf1171c08a89602df48cf7387
Alissa-MG <alissagarcia10118@gmail.com>
2024-09-11 20:35:26 -0500

    intial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const guessedLettersElement = document.querySelector(".guessed-letters");
+const guessLetterButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuessesElement = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const inUse = function (word) {
+   const inUseLetters =  [];
+   for (const letter of word) {
+    console.log(letter);
+    inUseLetters.push("●");
+   }
+   wordInProgress.innerText = inUseLetters.join("");
+}
+guessLetterButton.addEventListener("click", function (e) {
+   e.preventDefault();
+   const guess = letterInput.value;
+   console.log(guess);
+   letterInput.value = "";
+});
+
+inUseLetters(word);
